ed5a6c67402c39034cffcded68dcafade3c186f9
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-01-10 14:37:19 +0530

    Code

---
+++ precache-manifest.a4f98ee2477ff44a2c35a1590c947d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f23c8f90aa8fd0480107",
+    "url": "/static/js/main.f23c8f90.chunk.js"
+  },
+  {
+    "revision": "5cb9b55e9859431b45ee",
+    "url": "/static/js/1.5cb9b55e.chunk.js"
+  },
+  {
+    "revision": "f23c8f90aa8fd0480107",
+    "url": "/static/css/main.d4c71256.chunk.css"
+  },
+  {
+    "revision": "f9575af9ba1b86f43cb96d7d15884de6",
+    "url": "/index.html"
+  }
+];
